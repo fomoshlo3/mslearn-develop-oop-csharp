d2664024971fcfac0a9c30b22d19dee6c631edf7
Chris Howd <cahowd@microsoft.com>
2025-02-27 09:23:24 -0800

    adding LP4 M3 and M4 placeholders

---
+++ DownloadableCodeProjects/LP4_manage-app-data/Data_M3/Starter/Interfaces/IYearlyReportGenerator.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Data_M2;
+
+public interface IYearlyReportGenerator
+{
+    void GeneratePreviousYearReport(BankCustomer bankCustomer, int accountNumber, DateOnly reportDate);
+    void GenerateCurrentYearToDateReport(BankCustomer bankCustomer, int accountNumber, DateOnly reportDate);
+    void GenerateLast365DaysReport(BankCustomer bankCustomer, int accountNumber, DateOnly reportDate);
+}
